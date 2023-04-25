909ac285f8e2559fd9ac5ed44c7d751c55c319f2
Artem Golubev <lesovba@gmail.com>
2023-04-25 11:35:46 +0500

    Created C# environment for Task1

---
+++ ShowSecondDigit.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int GetThreeDigitNumber()
+{
+    int rand = new Random().Next(100, 1000);
+    Console.WriteLine(rand);
+    return rand;
+}
+
+int ShowSecondDigit(int num)
+{
+    int second_digit = num%100;
+    Console.WriteLine("Вторая цифра числа равна" + second_digit);
+    return second_digit;
+}
+
+ShowSecondDigit(GetThreeDigitNumber()); 
