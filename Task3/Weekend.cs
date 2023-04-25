a7de1975fd794700b03fcf5f7b9f5eb79b14bd82
Artem Golubev <lesovba@gmail.com>
2023-04-25 13:35:14 +0500

    Completed Task3

---
+++ Task3/Weekend.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите день недели от 1 до 7");
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+void CheckWeekend (int num) {
+    if (num == 6 || num == 7) 
+    {
+        Console.WriteLine(num + "->да");
+    }
+    else 
+    {
+        Console.WriteLine(num + "->нет");
+    }
+}
+
+CheckWeekend(dayOfWeek);
